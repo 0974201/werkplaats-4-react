cd5dd869b24260f04975f88166e79155731fbd3b
Yong Pok Low <1053079@hr.nl>
2023-04-15 00:59:20 +0200

    Just playing with JSX..

--- feedback-tool/src/index.js
+++ feedback-tool/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import logo from './SuperVegeta.gif';
+import './App.css';
 
-const App = () => {
-    return <h1>Hello Dog</h1>
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App_header'>
+                <div className='gif_text'>
+                    <h1>Suiker Suiker Suiker</h1>
+                </div>
+                <div className='gif_container'>
+                    <img src={logo} alt='Smug blonde monkeyman pointing at himself' style={{ position: 'absolute', zIndex: -1 }}></img>
+                    <h2 style={{ zIndex: 1 }}>Doogggg</h2>
+                </div>
+            </header>
+        </div>
+
+    )
 }
 
 const root = document.getElementById('root');
