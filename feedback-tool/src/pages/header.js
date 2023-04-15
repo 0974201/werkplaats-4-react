99d637e972d58340e4067c40601ad52da86b798b
Yong Pok Low <1053079@hr.nl>
2023-04-15 15:53:22 +0200

    smacked in dyflexis svg

--- feedback-tool/src/pages/header.js
+++ feedback-tool/src/pages/header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 function Header() {
     return (
         <nav>
             <div classname='header'>
                 <div className='header_information'>
+                    <img className="SVG" width="208" height="50" src="https://www.dyflexis.com/wp-content/uploads/2019/04/logo-dyflexis-2.svg"></img>
                     <div className='header_links'>
                         <Link to='/' className='link' style={{ marginLeft: '10px', marginRight: '20px' }}>Home</Link>
                         <Link to='./About' className='link' style={{ marginRight: '20px' }}>About us</Link>
