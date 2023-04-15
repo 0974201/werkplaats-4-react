35bbad27ec6d59ed7f0420794b6d8614ab114bb3
Yong Pok Low <1053079@hr.nl>
2023-04-15 13:54:29 +0200

    survey page

--- feedback-tool/src/index.js
+++ feedback-tool/src/index.js
@@ -8,6 +8,7 @@ import { Contact } from './pages/contact';
 import { About } from './pages/about';
 import { Login } from './pages/login';
 import { NotFound } from './pages/NotFound';
+import { Survey } from './pages/survey';
 import './css/App.css';
 
 
@@ -24,6 +25,7 @@ function App() {
                 <Route path='/contact' element={<Contact />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/login' element={<Login />} />
+                <Route path='/survey' element={<Survey />} />
                 <Route path='/*' element={<NotFound />} />
             </Routes>
         </>
---
+++ feedback-tool/src/pages/survey.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function Survey() {
+    return (
+        <h1>Survey</h1>
+    )
+
+}
