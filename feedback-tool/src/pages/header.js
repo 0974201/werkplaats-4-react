1b9344e0c55225a01fb11a4b82484f2139cd335c
Yong Pok Low <1053079@hr.nl>
2023-04-15 03:10:54 +0200

    yup more css

--- feedback-tool/src/pages/header.js
+++ feedback-tool/src/pages/header.js
@@ -4,13 +4,17 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <nav>
-            <div className='header_information'>
-                <Link to='./Home'>Home</Link>
-                <Link to='./About'>About us</Link>
-                <Link to='./contact'>Contact Us</Link>
-            </div>
-            <div className='nav_login'>
-                <Link to='./Login'>Login</Link>
+            <div classname='header'>
+                <div className='header_information'>
+                    <div className='header_links'>
+                        <Link to='./Home' style={{ marginLeft: '10px', marginRight: '20px' }}>Home</Link>
+                        <Link to='./About' style={{ marginRight: '20px' }}>About us</Link>
+                        <Link to='./contact' style={{ marginRight: '20px' }}>Contact Us</Link>
+                    </div>
+                    <div className='nav_login'>
+                        <Link to='./Login'>Login</Link>
+                    </div>
+                </div>
             </div>
         </nav>
     )
