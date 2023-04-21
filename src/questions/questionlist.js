b735893fe7618fc8ff2f85ddffd1a3f2d0ebf954
Yong Pok Low <1053079@hr.nl>
2023-04-22 00:57:11 +0200

    removed component and using question.map ?

--- src/questions/questionlist.js
+++ src/questions/questionlist.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { useState } from 'react';
 import './questions.css';
-import Survey2 from '../survey/survey'
+import Survey2, { questions } from '../survey/survey'
 
 
-
-function Questions() {
+function ModifyQuestion() {
     const [name, setName] = useState('');
+    const [question, modifyQuestion] = useState(questions);
+    let nextId = 0
 
     return (
         <div>
             <h1> Questions </h1>
-            <Survey2 />
-            <button>Click here</button>
-            <input value={name} onChange={e => setName(e.target.value)} />
+
+            {question.map(item => (
+                <div key={item.id}>{item.id + '. ' + item.question}<button>modify</button>
+                </div>
+            )
+            )}
+            {/* <button onClick={() => {
+                modifyQuestion([
+                    ...question,
+                    { id: nextId++, question: name }
+                ]);
+            }}>Add</button> */}
+            {/* {question.map(item => (
+                <div key={item.id}>{item.name}</div>
+            ))} */}
         </div>
     )
 }
 
-export default Questions
+export default ModifyQuestion
--- src/survey/survey.js
+++ src/survey/survey.js
@@ -25,7 +25,7 @@ function MultipleChoiceQuestion({ question }) {
     )
 }
 
-function Question({questions}) {
+function Question({ questions }) {
     const questionList = questions.map(question => {
         switch (question.type) {
             case 'MultipleChoice':
@@ -45,36 +45,37 @@ function Question({questions}) {
         <>
             {questionList}
         </>
-        )
+    )
 }
 
+export let questions = [
+    {
+        type: "MultipleChoice",
+        id: 1,
+        question: "Wat is de naam van je vis?",
+        options: ['Bubbles', 'John', 'Speedy', 'The drowned one']
+    },
+    {
+        type: "Open",
+        id: 2,
+        question: "Hoe heet je huis spin?",
+        options: null
+    },
+    {
+        type: "MultipleChoice",
+        id: 3,
+        question: "Wat is de naam van je kat?",
+        options: ['Scratch', 'Tiger', 'Spot', 'Nigel']
+    },
+    {
+        type: "Open",
+        id: 4,
+        question: "Van welke saus hou je?",
+        options: null
+    }
+]
+
 export default function Survey2() {
-    let questions = [
-        {
-            type: "MultipleChoice",
-            id: 1,
-            question: "Wat is de naam van je vis?",
-            options: ['Bubbles', 'John', 'Speedy', 'The drowned one']
-        },
-        {
-            type: "Open",
-            id: 2,
-            question: "Hoe heet je huis spin?",
-            options: null
-        },
-        {
-            type: "MultipleChoice",
-            id: 3,
-            question: "Wat is de naam van je kat?",
-            options: ['Scratch', 'Tiger', 'Spot', 'Nigel']
-        },
-        {
-            type: "Open",
-            id: 4,
-            question: "Van welke saus hou je?",
-            options: null
-        }
-    ]
 
     return (
         <div className={"survey"}>
