d028af9f591bc5121a9983a83d47f8f0454704cc
Sushi <31491170+0974201@users.noreply.github.com>
2023-04-23 00:49:11 +0200

    cnx met db
    Signed-off-by: Sushi <31491170+0974201@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// server shit
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -12,3 +13,19 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+//connectie met db
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize({ //kijken of ie mijn test db pakt
+  dialect: 'sqlite',
+  storage: '/database/test.db'
+});
+
+sequelize.authenticate().then(()=> {
+  console.log("shit leeft")
+}).catch((error)=>{
+  console.log(error);
+});
+
