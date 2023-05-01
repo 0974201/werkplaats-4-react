7ade18762e8a640aef7ad8952f69830a2c93d95b
Yong Pok Low <1053079@hr.nl>
2023-05-01 21:17:56 +0200

    checks for question type

--- src/questions/changequestion.js
+++ src/questions/changequestion.js
@@ -11,11 +11,11 @@ export default function ChangeQuestion({ question }) {
     console.log(question[id].question)
     console.log(id)
     console.log(question)
-
+    console.log(question[id].type)
 
     const handleModify = (id, newQuestion) => {
         console.log('dit woss ' + id, newQuestion);
-
+        console.log(value)
         let updatedQuestion = questionlist.map(question => {
             if (question.id === id) {
                 return { ...question, question: newQuestion };
@@ -29,12 +29,25 @@ export default function ChangeQuestion({ question }) {
     };
 
     console.log(questionlist)
+    /* Checks for whether the question type is Open or Multiple Choice */
     return (
         <div>
-            <h1> Change Question {id}</h1>
-            <p>{questionlist[id].question}</p>
-            <input type='text' className='input' value={value} onChange={e => setValue(e.target.value)}></input>
-            <button className='button' onClick={() => handleModify(id, value)}>Modify</button>
+            {question[id].type === 'Open' &&
+                <>
+                    <h1> Change Question {id}</h1>
+                    <p>{questionlist[id].question}</p>
+                    <input type='text' className='input' value={value} onChange={e => setValue(e.target.value)}></input>
+                    <button className='button' onClick={() => handleModify(id, value)}>Modify</button>
+                </>
+            }
+            {question[id].type === 'MultipleChoice' &&
+                <>
+                    <h1> Change Multiple Choice Question {id}</h1>
+                    <p>{questionlist[id].question}</p>
+                    <input type='text' className='input' value={value} onChange={e => setValue(e.target.value)}></input>
+                    <button className='button' onClick={() => handleModify(id, value)}>Modify</button>
+                </>
+            }
         </div>
     )
 }
