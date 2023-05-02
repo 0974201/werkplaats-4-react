e77859946c343dece18356371e0cfacc859d7859
Yong Pok Low <1053079@hr.nl>
2023-05-02 02:13:17 +0200

    disabled next button if there is no next question

--- src/questions/changequestion.js
+++ src/questions/changequestion.js
@@ -4,18 +4,13 @@ import { useParams, Link } from 'react-router-dom';
 import { questions } from '../index.js'
 
 
-
 export default function ChangeQuestion({ question }) {
     const [value, setValue] = useState('')
     const { id } = useParams();
 
     const [questionlist, setQuestion] = useState(questions);
     const [option, setSelectedOption] = useState(questions);
-    console.log(question[id].question)
-    console.log(id)
-    console.log(question)
-    console.log(question[id].type)
-    console.log('heyy dit zijn ' + question[id].options)
+
 
     const handleModify = (id, newQuestion) => {
         console.log('dit woss ' + id, newQuestion);
@@ -27,13 +22,10 @@ export default function ChangeQuestion({ question }) {
                 return question;
             }
         });
-        console.log('dit is ' + setQuestion)
-        setQuestion(updatedQuestion);
-        question[id].question = newQuestion;
+
     };
 
-    console.log('options' + question[id].options)
-    console.log('option' + question[id].option)
+
     /* Checks for whether the question type is Open or Multiple Choice */
     return (
         <div>
@@ -76,11 +68,15 @@ export default function ChangeQuestion({ question }) {
                     </div>
                 </>
             }
-            {console.log(question[id].id + 1)}
-            {console.log('id lengthh' + question[id].id.length)}
+
+            {/* buttons for Previous and Next Id's in the Array. */}
 
             <Link to={id > 0 ? `/question/${question[id].id - 1}` : ''}><button disabled={id === '0'} className='button'>Previous</button></Link>
-            <Link to={`/question/${question[id].id + 1}`}><button className='button'>Next</button></Link>
+            {id < (questionlist.length - 1) ? /* if current id is lower than the questionlist array (-1  due to index!) */
+                <Link to={`/question/${question[id].id + 1}`}><button className='button'>Next</button></Link>
+                : /* button is disabled if there is no more questions with a higher id in the array.*/
+                <button disabled className='button'>Next</button>
+            }
         </div >
     )
 }
