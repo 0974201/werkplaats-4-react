feeb63ca4daa89e2a3c1cf52d40e954547e8abdf
Yong Pok Low <1053079@hr.nl>
2023-05-06 19:02:33 +0200

    smacked the question types into a function

--- src/questions/changequestion.js
+++ src/questions/changequestion.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './questions.css';
 import { useParams, Link } from 'react-router-dom';
 import { questions } from '../index.js'
@@ -44,30 +44,21 @@ export default function ChangeQuestion({ question, options }) {
         })
         setRadioValue(newRadio)
     }
-
-    /* Checks for whether the question type is Open or Multiple Choice */
-
-    return (
-        <div>
-            {question[id].type === 'Open' &&
+    function renderQuestion() {
+        const question = questions[id];
+        if (question.type === 'Open') {
+            return (
                 <>
                     <h1> Change Question {id}</h1>
                     <p><b>{questionlist[id].question}</b></p>
-                    <div className='save_question_border'>
-                        <div className='save_question_box'>
-                            <textarea type='text' className='textarea' value={questionvalue} onChange={(e) => {
-                                console.log(value); setQuestionValue(e.target.value)
-                            }}></textarea>
-                            <button className='button' onClick={() => handleModify(question[id].id, questionvalue)}>Modify</button>
-
-                            <button className='button'>Save</button>
-                        </div>
-                    </div>
                 </>
-            }
-            {question[id].type === 'MultipleChoice' &&
-                <>
-                    <h1> Change Multiple Choice Question {id}</h1>
+            )
+        }
+
+        else if (question.type === 'MultipleChoice') {
+            return (
+                <div>
+                    <h1>Change Multiple Choice Question {id}</h1>
                     <p><b>{questionlist[id].question}</b></p>
                     {questionlist[id].options.map((option, optionIndex) => {
                         return (
@@ -83,32 +74,48 @@ export default function ChangeQuestion({ question, options }) {
                                             defaultValue={option}
                                             onChange={event => ReplaceRadio(id, optionIndex, event.target.value)}>
                                         </input>
-
                                     ) : (
                                         <label onClick={handleClick}>{option}</label>
                                     )}
                                 </div>
                             </div>
-                        );
+                        )
                     })}
-                    <div className='save_question_border'>
-                        <div className='save_question_box'>
-                            <textarea type='text' className='textarea' defaultValue={questionvalue} onChange={(e) => setQuestionValue(e.target.value)}></textarea>
-                            <button className='button' onClick={() => handleModify(question[id].id, questionvalue)}>Modify</button>
-                            <button className='button'>Save</button>
-                        </div>
-                    </div>
-                </>
-            }
+                </div>
+            )
+        }
+    }
+
+    /* re-renders the question in the textarea depending on the id parameter. */
+    useEffect(() => {
+        setQuestionValue(question[id].question);
+    }, [id]);
+
+    /* Checks for whether the question type is Open or Multiple Choice */
+
+    return (
+        <div>
+            {renderQuestion()}
+            <div className='save_question_border'>
+                <div className='save_question_box'>
+                    <textarea type='text' className='textarea' defaultValue={questionvalue} onChange={(e) => setQuestionValue(e.target.value)}></textarea>
+                    <button className='button' onClick={() => handleModify(question[id].id, questionvalue)}>Modify</button>
+                    <button className='button'>Save</button>
+                </div>
+            </div>
+
+
 
             {/* buttons for Previous and Next Id's in the Array. */}
 
             <Link to={id > 0 ? `/question/${question[id].id - 1}` : ''}><button disabled={id === '0'} className='button'>Previous</button></Link>
-            {id < (questionlist.length - 1) ? /* if current id is lower than the questionlist array (-1  due to index!) */
-                <Link to={`/question/${question[id].id + 1}`}><button className='button'>Next</button></Link>
-                : /* button is disabled if there is no more questions with a higher id in the array.*/
-                <button disabled className='button'>Next</button>
+            {
+                id < (questionlist.length - 1) ? /* if current id is lower than the questionlist array (-1  due to index!) */
+                    <Link to={`/question/${question[id].id + 1}`}><button className='button'>Next</button></Link>
+                    : /* button is disabled if there is no more questions with a higher id in the array.*/
+                    <button disabled className='button'>Next</button>
             }
-        </div >
+        </div>
     )
 }
+
