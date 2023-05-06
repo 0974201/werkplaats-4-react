58eceeb3c208fa3f27bdc6e254ece7561d907c30
Sushi <31491170+0974201@users.noreply.github.com>
2023-05-06 23:03:43 +0200

    💬
    Signed-off-by: Sushi <31491170+0974201@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express'); // server shit
 const db = require('./database/db.js') // connectie met db
+//const getBirb = require('./database/models/taebl.js');
+//const getGaem = require('./database/models/taebl.js');
 
 const app = express();
 
@@ -19,6 +21,37 @@ app.get("/test", function(req, res){
     }
     console.log(row);
     res.json(row);
+  });
+});
+
+app.get("/test_birb", function(req, res){
+  db.all('SELECT * FROM vogels', (err, row) => {
+    if (err){
+      throw new Error(err.message);
+    }
+    console.log(row);
+    res.json(row);
+  });
+});
+
+app.get("/test_gaem", function(req, res){
+  db.all('SELECT * FROM games', (err, row) => {
+    if (err){
+      throw new Error(err.message);
+    }
+    res.json(row);
+    console.log(row);
+  });
+});
+
+
+app.get("/test_map", function(req, res){ //hmmm
+  db.map('SELECT * FROM games', (err, row) => {
+    if (err){
+      throw new Error(err.message);
+    }
+    res.json(row);
+    console.log(row);
   });
 });
 
