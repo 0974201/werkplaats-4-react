1c0d4c97e88067ba0e249900f3bde253c8e0a925
Zasha <0925315@hr.nl>
2023-05-09 15:01:39 +0200

    focus verliezen tijdens het type in de textarea gefixet

--- src/survey/survey.js
+++ src/survey/survey.js
@@ -4,7 +4,41 @@ import React, {useState} from "react";
 export default function Survey2({questionsArray}) {
     const [answeredArray, setAnsweredArray] = useState(questionsArray.map(question => {return {...question, answer: 'hi'}}))
     const [questionShown, setQuestionShow] = useState(0)
-    console.log(answeredArray)
+
+    const questionList = answeredArray.map((question, questionIndex) => {
+            switch (question.type) {
+                case 'MultipleChoice':
+                    return (
+                        <div>
+                            <h3>{question.question}</h3>
+                            <ul>
+                                {question.options.map((option, optionIndex) =>
+                                    <li key={optionIndex}>
+                                        <label>
+                                            <input type={"radio"} value={option} name={"question" + question.id} />
+                                            {option}
+                                        </label>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    )
+                case 'Open':
+                    return (
+                        <div>
+                            <h3>{question.question}</h3>
+                            <textarea
+                                maxLength={250}
+                                value={answeredArray[questionIndex].answer}
+                                onChange={e => replaceAnswer(questionIndex, e.target.value)}
+                            />
+                        </div>
+                    )
+                default:
+                    console.log("Wrong type")
+            }
+        }
+    )
 
     function replaceAnswer(questionIndex, value) {
         const inBetweenArray = answeredArray.map((question, i) => {
@@ -17,59 +51,8 @@ export default function Survey2({questionsArray}) {
         })
         setAnsweredArray(inBetweenArray)
     }
-
-    function OpenQuestion({question, questionIndex}) {
-        console.log(question)
-        return (
-            <div>
-                <h3>{question.question}</h3>
-                <textarea
-                    maxLength={250}
-                    value={answeredArray[questionIndex].answer}
-                    onChange={e => replaceAnswer(questionIndex, e.target.value)}
-                />
-            </div>
-        )
-    }
-
-    function MultipleChoiceQuestion({question}) {
-        return (
-            <div>
-                <h3>{question.question}</h3>
-                <ul>
-                    {question.options.map((option, optionIndex) =>
-                        <li key={optionIndex}>
-                            <label>
-                                <input type={"radio"} value={option} name={"question" + question.id} />
-                                {option}
-                            </label>
-                        </li>
-                    )}
-                </ul>
-            </div>
-        )
-    }
-
-    function Question() {
-
-        console.log(questionShown)
-        console.log(answeredArray.length)
-        const questionList = answeredArray.map((question, questionIndex) => {
-                switch (question.type) {
-                    case 'MultipleChoice':
-                        return (
-                            <MultipleChoiceQuestion key={questionIndex} question={question} questionIndex={questionIndex} />
-                        )
-                    case 'Open':
-                        return (
-                            <OpenQuestion key={questionIndex} question={question} questionIndex={questionIndex} />
-                        )
-                    default:
-                        console.log("Wrong type")
-                }
-            }
-        )
-        return (
+    return (
+        <div className={"survey"}>
             <>
                 <h3>Vraag {questionShown+1}/{questionList.length}</h3>
                 {questionList[questionShown]}
@@ -84,12 +67,6 @@ export default function Survey2({questionsArray}) {
 
 
             </>
-        )
-    }
-
-    return (
-        <div className={"survey"}>
-            <Question />
         </div>
     )
 }
