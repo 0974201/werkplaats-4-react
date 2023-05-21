f14f15d48ea118582d99aa06617e1c53a31356a7
Sushi <31491170+0974201@users.noreply.github.com>
2023-05-22 01:33:34 +0200

    merge?
    Signed-off-by: Sushi <31491170+0974201@users.noreply.github.com>

--- server/routes/route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); // express
-const routes = require('./../controllers/controller.js');
-
-const router = express.Router(); // router
-
-router.get('/tezt', routes.getGames);
-console.log(routes.getGames);
-
-module.exports = router;
