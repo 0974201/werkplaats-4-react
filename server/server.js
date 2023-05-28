910d1a6416a63e4ba8ed8b6c388c09b393b55ed6
Zasha <0925315@hr.nl>
2023-05-28 13:23:09 +0200

    elke open vraag word opgeslagen in de database. nu nog multiple choice doen

--- server/server.js
+++ server/server.js
@@ -15,58 +15,54 @@ app.get("/", function(req, res){
 });
 
 app.post('/api/saveNewSurvey', bodyParser.json(), async function (req, res) {
-
     try {
         // starting a SQL transaction
         await runQuery('BEGIN')
-
+        // inserting a survey and getting the inserted id back
+        const surveyId = await insertAndGetLastId("INSERT INTO survey (description, open_date, close_date) VALUES (?, ?, ?)",
+            [req.body.description, req.body.openDate, req.body.closeDate])
+        // commit the started SQL transaction
+        await runQuery('COMMIT');
         try {
-            // inserting a survey and getting the inserted id back
-            const surveyId = await insertAndGetLastId("INSERT INTO survey (description, open_date, close_date) VALUES (?, ?, ?)",
-                [req.body.description, req.body.openDate, req.body.closeDate])
-
             // starting a loop to insert every question in the database
-            await Promise.all(req.body.questions.map(async function(question, index){
+            for (const [index, question] of req.body.questions.entries()) {
 
-                //check in the question is a open or a multiple choice question
+                //check in the question is an open or a multiple choice question
                 if (question.type === 'Open') {
-
-                    // inserting a open question and getting the inserted id back so you can use it in the question query
+                    // starting a SQL transaction for every loop cylce
+                    await runQuery('BEGIN')
+                    // inserting an open question and getting the inserted id back so you can use it in the question query
                     const openQuestionId = await insertAndGetLastId("INSERT INTO open_question (question) VALUES (?)",
                         [question.question])
 
-                    // inserting the qusetion with the open question id and getting the question id back
+                    // inserting the question with the open question id and getting the question id back
                     const questionId = await insertAndGetLastId("INSERT INTO questions (Open_Question_ID, is_deleted) VALUES (?, ?)",
                         [openQuestionId, false])
 
                     // insert everythin in the filled_in table to combine everything
                     await runQuery("INSERT INTO filled_in (Survey_ID, Question_ID, question_order, is_reviewed) VALUES (?, ?, ?, ?)",
                         [surveyId, questionId, index, false],)
-
+                    // commit the started SQL transaction for every loop cycle
+                    await runQuery('COMMIT');
                 } else if (question.type === 'MultipleChoice') {
 
                 } else {
                     console.log('wrong type of question')
                 }
-            }))
-            // commit the started SQL transaction
-            await runQuery('COMMIT');
-
+            }
         } catch (error) {
-            // incase of a erron rollback the SQL transaction
+            // in case of an error rollback the SQL transaction
             await runQuery('ROLLBACK');
             console.log(error)
         }
     } catch (error) {
-
+        await runQuery('ROLLBACK');
     }
     // Helper function to put a insert and a last id together
     async function insertAndGetLastId(query, param) {
         await runQuery(query, param)
         return  await getLastInsertedId()
     }
-
-
     // Helper function to get the ID of the last inserted row
     function getLastInsertedId() {
         return new Promise((resolve, reject) => {
@@ -79,7 +75,6 @@ app.post('/api/saveNewSurvey', bodyParser.json(), async function (req, res) {
             });
         });
     }
-
     // Helper function to run a query with parameters
     function runQuery(sql, params) {
         return new Promise((resolve, reject) => {
@@ -92,7 +87,6 @@ app.post('/api/saveNewSurvey', bodyParser.json(), async function (req, res) {
             });
         });
     }
-
     res.send('saved')
 })
 
