06f191bb2a80808b973c2480ddcde3cc0adbdf1f
Yong Pok Low <1053079@hr.nl>
2023-05-29 16:50:35 +0200

    queries work.. needed correct time format

--- server/server.js
+++ server/server.js
@@ -14,82 +14,82 @@ app.get("/", function (req, res) {
 });
 
 app.post('/api/saveNewSurvey', bodyParser.json(), async function (req, res) {
+  try {
+    // starting a SQL transaction
+    await runQuery('BEGIN')
+    // inserting a survey and getting the inserted id back
+    const surveyId = await insertAndGetLastId("INSERT INTO survey (title, description, open_date, close_date, can_be_anonymous) VALUES (?, ?, ?)",
+      [req.body.title, req.body.description, req.body.openDate, req.body.closeDate, req.body.anonymity])
+    // commit the started SQL transaction
+    await runQuery('COMMIT');
     try {
-        // starting a SQL transaction
-        await runQuery('BEGIN')
-        // inserting a survey and getting the inserted id back
-        const surveyId = await insertAndGetLastId("INSERT INTO survey (title, description, open_date, close_date, can_be_anonymous) VALUES (?, ?, ?)",
-            [req.body.title, req.body.description, req.body.openDate, req.body.closeDate, req.body.anonymity])
-        // commit the started SQL transaction
-        await runQuery('COMMIT');
-        try {
-            // starting a loop to insert every question in the database
-            for (const [index, question] of req.body.questions.entries()) {
+      // starting a loop to insert every question in the database
+      for (const [index, question] of req.body.questions.entries()) {
 
-                //check in the question is an open or a multiple choice question
-                if (question.type === 'Open') {
-                    // starting a SQL transaction for every loop cycle
-                    await runQuery('BEGIN')
-                    // inserting an open question and getting the inserted id back, so you can use it in the question query
-                    const openQuestionId = await insertAndGetLastId("INSERT INTO open_question (question) VALUES (?)",
-                        [question.question])
+        //check in the question is an open or a multiple choice question
+        if (question.type === 'Open') {
+          // starting a SQL transaction for every loop cycle
+          await runQuery('BEGIN')
+          // inserting an open question and getting the inserted id back, so you can use it in the question query
+          const openQuestionId = await insertAndGetLastId("INSERT INTO open_question (question) VALUES (?)",
+            [question.question])
 
-                    // inserting the question with the open question id and getting the question id back
-                    const questionId = await insertAndGetLastId("INSERT INTO questions (Open_Question_ID, is_deleted) VALUES (?, ?)",
-                        [openQuestionId, false])
+          // inserting the question with the open question id and getting the question id back
+          const questionId = await insertAndGetLastId("INSERT INTO questions (Open_Question_ID, is_deleted) VALUES (?, ?)",
+            [openQuestionId, false])
 
-                    // insert everything in the filled_in table to combine everything
-                    await runQuery("INSERT INTO filled_in (Survey_ID, Question_ID, question_order) VALUES (?, ?, ?)",
-                        [surveyId, questionId, index])
-                    // commit the started SQL transaction for every loop cycle
-                    await runQuery('COMMIT');
-                } else if (question.type === 'MultipleChoice') {
+          // insert everything in the filled_in table to combine everything
+          await runQuery("INSERT INTO filled_in (Survey_ID, Question_ID, question_order) VALUES (?, ?, ?)",
+            [surveyId, questionId, index])
+          // commit the started SQL transaction for every loop cycle
+          await runQuery('COMMIT');
+        } else if (question.type === 'MultipleChoice') {
 
-                } else {
-                    console.log('wrong type of question')
-                }
-            }
-        } catch (error) {
-            // in case of an error rollback the SQL transaction
-            await runQuery('ROLLBACK');
-            console.log(error)
+        } else {
+          console.log('wrong type of question')
         }
+      }
     } catch (error) {
-        await runQuery('ROLLBACK');
+      // in case of an error rollback the SQL transaction
+      await runQuery('ROLLBACK');
+      console.log(error)
     }
-    // Helper function to put a insert and a last id together
-    async function insertAndGetLastId(query, param) {
-        await runQuery(query, param)
-        return  await getLastInsertedId()
-    }
-    // Helper function to get the ID of the last inserted row
-    function getLastInsertedId() {
-        return new Promise((resolve, reject) => {
-            db.get('SELECT last_insert_rowid() as id', (error, row) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(row.id);
-                }
-            });
-        });
-    }
-    // Helper function to run a query with parameters
-    function runQuery(sql, params) {
-        return new Promise((resolve, reject) => {
-            db.run(sql, params, function (error) {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(this);
-                }
-            });
-        });
-    }
-    res.send('saved')
+  } catch (error) {
+    await runQuery('ROLLBACK');
+  }
+  // Helper function to put a insert and a last id together
+  async function insertAndGetLastId(query, param) {
+    await runQuery(query, param)
+    return await getLastInsertedId()
+  }
+  // Helper function to get the ID of the last inserted row
+  function getLastInsertedId() {
+    return new Promise((resolve, reject) => {
+      db.get('SELECT last_insert_rowid() as id', (error, row) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(row.id);
+        }
+      });
+    });
+  }
+  // Helper function to run a query with parameters
+  function runQuery(sql, params) {
+    return new Promise((resolve, reject) => {
+      db.run(sql, params, function (error) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(this);
+        }
+      });
+    });
+  }
+  res.send('saved')
 })
 
-app.get("/test", function(req, res){
+app.get("/test", function (req, res) {
   //res.send('test'); < dit zorgde er voor dat het de hele tijd borkte. je kan maar 1x een send of json dingetje hebben.
   res.type('json');
   db.all('SELECT name FROM sqlite_schema', (err, row) => {
@@ -168,9 +168,18 @@ app.get("/api/questions", function (req, res) {
 app.get("/api/surveys", function (req, res) {
   res.type('json');
 
+  /* sets the query parameters in the url */
   const isOpen = req.query.open === 'true';
   const isClosed = req.query.open === 'false';
   const beingReviewed = req.query.open === 'reviewed';
+
+  /* formats the time to DD-MM-YY so it works with database times*/
+  let currentDate = new Date();
+  let formattedDate = currentDate.toLocaleDateString('en-US', {
+    day: '2-digit',
+    month: '2-digit',
+    year: '2-digit'
+  }).split('/').join('-'); // Convert slashes to dashes
 
 
   console.log(isOpen)
@@ -178,16 +187,15 @@ app.get("/api/surveys", function (req, res) {
   console.log('req query ' + req.query.close_date)
   console.log('req.query open ' + req.query.open_date)
   console.log(req.query.open)
-  let sql = `SELECT * FROM survey as s`;
+  let sql = `SELECT * FROM survey`;
   if (isOpen) {
-    sql += ` WHERE close_date > date('now')`;
+    sql += ` WHERE close_date > '${formattedDate}' AND is_reviewed = 0`;
   } else if (isClosed) {
-    sql += ` WHERE close_date < date('now')`;
+    sql += ` WHERE close_date < '${formattedDate}' AND is_reviewed = 0`;
   } else if (beingReviewed) {
-    sql += ` WHERE is_reviewed = 1 )`;
+    sql += ` WHERE is_reviewed = 1`;
   }
-
-
+  console.log(sql)
   console.log('this is sql ' + sql)
   db.all(sql, (err, rows) => {
     if (err) {
@@ -216,28 +224,17 @@ app.get('/api/users', function (req, res) {
 app.get('/api/filled_surveys', function (req, res) {
   res.type('json');
   const id = req.query.Question_ID;
-  let sql = `SELECT questions.Question_ID, open_question.Open_Question_ID, open_question.question, multiple_choice.Multiple_Choice_ID, multiple_choice.question
-  FROM questions 
-  LEFT JOIN open_question ON questions.Question_ID = open_question.open_Question_ID
-  LEFT JOIN multiple_choice ON questions.Question_ID = multiple_choice.multiple_Choice_ID`;
+  // let sql = `SELECT questions.Question_ID, open_question.Open_Question_ID, open_question.question, multiple_choice.Multiple_Choice_ID, multiple_choice.question
+  // FROM questions 
+  // LEFT JOIN open_question ON questions.Question_ID = open_question.open_Question_ID
+  // LEFT JOIN multiple_choice ON questions.Question_ID = multiple_choice.multiple_Choice_ID`;
 
-  db.run(
-    sql,
-    function (err) {
-      if (err) {
-        console.log(err.message)
-      } else {
-        console.log('survey id:' + this.lastID)
-        surveyId = this.lastID
-      }
-    }
-  )
   query = `SELECT * FROM filled_in`
   console.log('req query ' + req.query);
   console.log('req body' + req.body);
   console.log('question_ID' + id);
-  console.log(sql)
-  db.all(query, [id], (err, row) => {
+  console.log
+  db.all(query, (err, row) => {
     if (err) {
       console.log(err.message);
     }
