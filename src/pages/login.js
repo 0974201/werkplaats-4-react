eeed50f5e1b8b7da75e52cf4dba27617b058bf59
Sushi <31491170+0974201@users.noreply.github.com>
2023-06-09 21:26:15 +0200

    moved async func to login db
    and added the ugliest logout button ever

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import { useState } from "react";
-//import { UserLogin } from "../universal/loginDB";
+import { UserLogin } from "../universal/loginDB";
 import { useNavigate } from 'react-router';
-
-async function getUser(login){
-  return fetch('http://127.0.0.1:81/handle_login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(login)
-  })
-  .then(res => res.json());
-}
 
 export function Login() {
   const navigate = useNavigate();
@@ -21,7 +10,7 @@ export function Login() {
 
   const handleSubmit = async e => {
     e.preventDefault();
-    const user = await getUser({
+    const user = await UserLogin({
       email,
       password
     });
@@ -39,8 +28,7 @@ export function Login() {
       <h1><p className="login_header"><b>Log in</b></p></h1>
       <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail:</label> <br/>
-          <input type="email" placeholder="E-mail" onChange={e => setEmail(e.target.value)} pattern="/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
-          required /> <br/>
+          <input type="email" placeholder="E-mail" onChange={e => setEmail(e.target.value)} id="email" required /> <br/>
           <label htmlFor="password">Password:</label> <br/>
           <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} /> <br/><br/>
           <input type="submit" className="submit-btn5"/>
--- src/universal/loginDB.js
+++ src/universal/loginDB.js
@@ -1,14 +1,12 @@
-function UserLogin(user){
-  fetch('http://localhost:81/handle_login', {
+async function UserLogin(login){
+  return fetch('http://127.0.0.1:81/handle_login', {
     method: 'POST',
-    mode: 'cors',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(user)
-  });
-  console.log(user);
-  
+    body: JSON.stringify(login)
+  })
+  .then(res => res.json());
 }
 
 export { UserLogin }
